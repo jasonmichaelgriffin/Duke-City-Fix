fefb0cd345e5a2ce5966ee10316b8fdbf6e4a78d
Jason Griffin <jasonmichaelgriffin@gmail.com>
2015-04-15 11:24:19 -0600

    Pushing In-Progress File (commit/push "early and often").

--- specification/personas.php
+++ specification/personas.php
@@ -5,8 +5,32 @@
 		<title>Personas</title>
 	</head>
 	<body>
-		<h1>Personas Mocked Up based on Duke City Fix Website</h1>
-			<p></p>
+		<h1><b>Personas Mocked Up based on Duke City Fix Website</b></h1>
+			<p>Here I've created two fictional personas per the class assignment</br></p>
+		<h2><b>Persona 1 "The Content Creator"</b></h2>
+			<p>Name - Jane Baca</br>
+			Age - 34</br>
+			Education - College Graduate</br>
+			Ethnicity - Hispanic</br>
+			Primary Access Method - Desktop</br>
+			Goals for accessing the site:</br>
+				Posting her own local interest photos</br>
+				Read featured blog posts</br>
+				</br>	</p>
+		<h3><b>Persona 2 "The Lurker"<b></b></h3>
+			<p>Name - John Smith</br>
+			Age - 39</br>
+			Education - Some College</br>
+			Ethnicity - White</br>
+			Primarily access on
+
+
+
+
+
+
+
+			</p>
 	</body>
 </html>
 /**
