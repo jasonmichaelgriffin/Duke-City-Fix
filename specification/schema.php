4bed4b4389838c5c495e4fb180d56a6c6cca1547
Jason Griffin <jasonmichaelgriffin@gmail.com>
2015-04-15 13:47:31 -0600

    1st draft of schema.php.  Untested/polished.

---
+++ specification/schema.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Conceptual Model</title>
+	</head>
+	<body>
+		<h1><b><i>Conceptual Model</i> Mocked Up Based on Duke City Fix Website (DCF)</b></h1>
+			<p>A fictional <i>conceptual</i> model I've created for DC<br/></p>
+		<h2><b>User Profile Details</b></h2>
+			<p>User Data Required to make create content and comment</p>
+			<ul>
+				<li>E-mail address</i>
+				<i>Full Name (one field)</li>
+			</ul>
+			<p>User Data Required by site but unnecessary to create content and comment</p>
+			<ul>
+				<i>Gender</i>
+				<i>Country</i>
+				<i>Zip</i>
+				<i>About Me Comment (displayed with blog posts)</i>
+				<i>Neighborhood ("where I live")</i>
+				<i>What Am I (business vs person)</i>
+			</ul>
+		<h3><b>Blog Details</b></h3>
+			<p>Data required for a blog post</p>
+			<ul>
+				<i>User Full Name</i>
+				<i>Blog Title</i>
+				<i>Content (text, photos, etc.)</i>
+				<i>Publish Date/time (user controlled)</i>
+			</ul>
+			<h4><b>Blog Comment</b></h4>
+				<p>Data required for a blog comment</p>
+				<ul>
+					<i>User Full Name</i>
+					<i>Blog Title</i>
+					<i>Comment (text)</i>
+					<i>Timestamp (not user controlled)</i>
+				</ul>
+	</body>
+</html>
