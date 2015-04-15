7bd0735c03a4f5c7c24e20fd15f07de7e2f2be50
Jason Griffin <jasonmichaelgriffin@gmail.com>
2015-04-15 11:29:28 -0600

    Initial Draft of personas.php
    Next step is to format it a bit better after testing.

--- specification/personas.php
+++ specification/personas.php
@@ -6,31 +6,27 @@
 	</head>
 	<body>
 		<h1><b>Personas Mocked Up based on Duke City Fix Website</b></h1>
-			<p>Here I've created two fictional personas per the class assignment</br></p>
+			<p>Here I've created two fictional personas per the class assignment<br/></p>
 		<h2><b>Persona 1 "The Content Creator"</b></h2>
-			<p>Name - Jane Baca</br>
-			Age - 34</br>
-			Education - College Graduate</br>
-			Ethnicity - Hispanic</br>
-			Primary Access Method - Desktop</br>
-			Goals for accessing the site:</br>
-				Posting her own local interest photos</br>
-				Read featured blog posts</br>
-				</br>	</p>
+			<p>Name - Jane Baca<br/>
+			Age - 34<br/>
+			Education - College Graduate<br/>
+			Ethnicity - Hispanic<br/>
+			Primary Access Method - Desktop<br/>
+			Goals for accessing the site:<br/>
+				Posting her own local interest photos<br/>
+				Read featured blog posts<br/>
+				<br/>	</p>
 		<h3><b>Persona 2 "The Lurker"<b></b></h3>
-			<p>Name - John Smith</br>
-			Age - 39</br>
-			Education - Some College</br>
-			Ethnicity - White</br>
-			Primarily access on
-
-
-
-
-
-
-
-			</p>
+			<p>Name - John Smith<br/>
+			Age - 39<br/>
+			Education - Some College<br/>
+			Ethnicity - White<br/>
+			Primarily accesses on mobile device (iPad)<br/>
+			Goals for accessing the site:<br/>
+			Read posts from other locals with his same interest (urban goat and chicken farming)<br/>
+			Reads the Morning Fix daily post<br/>
+		</p>
 	</body>
 </html>
 /**
