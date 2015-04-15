84c177ccdad0668367830c927e21f2f2e4034630
Jason Griffin <jasonmichaelgriffin@gmail.com>
2015-04-15 11:11:26 -0600

    Pushing In-Progress File (commit/push "early and often").

---
+++ specification/personas.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+			<meta charset="utf-8"/>
+		<title>Personas</title>
+	</head>
+	<body>
+		<h1>Personas Mocked Up based on Duke City Fix Website</h1>
+			<p></p>
+	</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: Jason Griffin
+ * Date: 4/15/2015
+ * Time: 9:41 AM
+ */
---
+++ specification/usecase.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head> </head>
+</html>
+ * Created by PhpStorm.
+ * User: Jason Griffin
+ * Date: 4/15/2015
+ * Time: 9:41 AM
+ */
