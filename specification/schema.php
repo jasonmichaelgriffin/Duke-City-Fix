67e74b78979e603902df7dedfc422224a44ba359
Jason Griffin <jasonmichaelgriffin@gmail.com>
2015-04-15 13:56:26 -0600

    2nd draft of schema.  Still being created.
    Will cut out "group forums" to keep scope reasonable.

--- specification/schema.php
+++ specification/schema.php
@@ -5,7 +5,7 @@
 		<title>Conceptual Model</title>
 	</head>
 	<body>
-		<h1><b><i>Conceptual Model</i> Mocked Up Based on Duke City Fix Website (DCF)</b></h1>
+		<h1><b><i>Conceptual Model</i> Mocke Up Based on Duke City Fix Website (DCF)</b></h1>
 			<p>A fictional <i>conceptual</i> model I've created for DC<br/></p>
 		<h2><b>User Profile Details</b></h2>
 			<p>User Data Required to make create content and comment</p>
@@ -38,5 +38,17 @@
 					<li>Comment (text)</li>
 					<li>Timestamp (not user controlled)</li>
 				</ul>
+		<h5><b>Relationships</b></h5>
+		<p>Notes on relationships</p>
+		<ul>
+			<li>Each user can have only one profile (uniquely defined by email address and name)</li>
+			<li>Each user can author many blog posts</li>
+			<li>Each blog post can have only one author</li>
+			<li>Each blog post can have many comments</li>
+			<li>Each user can author many comments</li>
+			<li>Each comment can have only one author</li>
+			<li>Each comment can be associated with only one blog post</li>
+			<li></li>
+		</ul>
 	</body>
 </html>
