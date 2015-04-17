b073f9b377fdf4ff8f592d4597080a0fca6cb0a2
Jason Griffin <jasonmichaelgriffin@gmail.com>
2015-04-17 14:51:53 -0600

    Updated diagram, removing superfluous field userName.  The sql statement file is still a work in progress.

--- sql/duke-city-fix.sql
+++ sql/duke-city-fix.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS blogPost;
 DROP TABLE IF EXISTS blogComment;
 
 CREATE TABLE profile (
-  userID INT UNSIGNED AUTO-INCREMENT NOT NULL ,
+  userId INT UNSIGNED AUTO-INCREMENT NOT NULL ,
   eMail VARCHAR(40) NOT NULL,
   userName VARCHAR(30) NOT NULL,
   country VARCHAR(3),
@@ -18,17 +18,26 @@ CREATE TABLE profile (
 );
 
 CREATE TABLE blogPost (
-	blogID INT UNSIGNED AUTO-INCREMENT NOT NULL,
-	userID INT UNSIGNED NOT NULL,
+	blogId INT UNSIGNED AUTO-INCREMENT NOT NULL,
+	userId INT UNSIGNED NOT NULL,
 	blogTitle varchar (100) NOT NULL,
 	content MEDIUMTEXT,
 	publishDate DATETIME NOT NULL,
-	userName VARCHAR (30) NOT NULL,  /*this field is coming from the profile table, how should I be handling it?
-	It's not really a foreign key because it's not a primary on the other table, so how do I link them?*/
 	INDEX (publishDate),
 	INDEX (userID),
-	UNIQUE (blogTitle),
+	INDEX (blogTitle),
 	PRIMARY KEY (blogID),
 	FOREIGN KEY (userID) REFERENCES profile(userID),
+;
+
+CREATE TABLE blogComment (
+	commentId INT UNSIGNED AUTO-INCREMENT NOT NULL,
+	userId INT NOT NULL
+
+
+
+
+
+);
 
 
