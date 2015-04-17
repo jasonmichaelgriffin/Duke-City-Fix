9dcd34abf9699c7affeaadf0f61ccd8b4ec9319b
Jason Griffin <jasonmichaelgriffin@gmail.com>
2015-04-17 14:29:52 -0600

    Updated the diagram, removing the User Name field from the blog table as it was not necessary.

--- sql/duke-city-fix.sql
+++ sql/duke-city-fix.sql
@@ -3,14 +3,32 @@ DROP TABLE IF EXISTS blogPost;
 DROP TABLE IF EXISTS blogComment;
 
 CREATE TABLE profile (
-  userID INT UNSIGNED PRIMARY KEY AUTO-INCREMENT NOT NULL ,
+  userID INT UNSIGNED AUTO-INCREMENT NOT NULL ,
   eMail VARCHAR(40) NOT NULL,
   userName VARCHAR(30) NOT NULL,
   country VARCHAR(3),
   postalCode INT UNSIGNED,
   neighborhood VARCHAR(100),
   aboutMeComment TINYTEXT,
-  businessOrPerson VARCHAR(8)
+  businessOrPerson VARCHAR(8),
   gender VARCHAR(1),
-/In "real life" I would have both businessOrPerson and gender be foreign keys attached to tables with just the values I want to accept limiting the valid options to the end user.
+  PRIMARY KEY(userID),
+  UNIQUE(email),
+/*In "real life" I would have both businessOrPerson and gender be foreign keys attached to tables with just the values I want to accept limiting the valid options to the end user.*/
 );
+
+CREATE TABLE blogPost (
+	blogID INT UNSIGNED AUTO-INCREMENT NOT NULL,
+	userID INT UNSIGNED NOT NULL,
+	blogTitle varchar (100) NOT NULL,
+	content MEDIUMTEXT,
+	publishDate DATETIME NOT NULL,
+	userName VARCHAR (30) NOT NULL,  /*this field is coming from the profile table, how should I be handling it?
+	It's not really a foreign key because it's not a primary on the other table, so how do I link them?*/
+	INDEX (publishDate),
+	INDEX (userID),
+	UNIQUE (blogTitle),
+	PRIMARY KEY (blogID),
+	FOREIGN KEY (userID) REFERENCES profile(userID),
+
+
