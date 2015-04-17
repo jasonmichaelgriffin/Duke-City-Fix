0f48b195b36ac3d4bd282f92b840ceedda6a666a
Jason Griffin <jasonmichaelgriffin@gmail.com>
2015-04-17 15:16:25 -0600

    Updated diagram again; changed comment entity to strong, removed blogTitle from it, and added blogId to it.
    Also completed sql (1st try)

--- sql/duke-city-fix.sql
+++ sql/duke-city-fix.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS Profile;
+DROP TABLE IF EXISTS dcfProfile;
 DROP TABLE IF EXISTS blogPost;
 DROP TABLE IF EXISTS blogComment;
 
-CREATE TABLE profile (
-  userId INT UNSIGNED AUTO-INCREMENT NOT NULL ,
+CREATE TABLE dcfProfile (
+  userId INT UNSIGNED AUTO_INCREMENT NOT NULL ,
   eMail VARCHAR(40) NOT NULL,
   userName VARCHAR(30) NOT NULL,
   country VARCHAR(3),
@@ -12,32 +12,35 @@ CREATE TABLE profile (
   aboutMeComment TINYTEXT,
   businessOrPerson VARCHAR(8),
   gender VARCHAR(1),
-  PRIMARY KEY(userID),
-  UNIQUE(email),
-/*In "real life" I would have both businessOrPerson and gender be foreign keys attached to tables with just the values I want to accept limiting the valid options to the end user.*/
+  PRIMARY KEY(userId),
+  UNIQUE(eMail),
+  UNIQUE (userName)/*In "real life" I would have both businessOrPerson and gender be foreign keys attached to tables with just the values I want to accept limiting the valid options to the end user.*/
 );
 
 CREATE TABLE blogPost (
-	blogId INT UNSIGNED AUTO-INCREMENT NOT NULL,
+	blogId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	userId INT UNSIGNED NOT NULL,
 	blogTitle varchar (100) NOT NULL,
 	content MEDIUMTEXT,
 	publishDate DATETIME NOT NULL,
 	INDEX (publishDate),
-	INDEX (userID),
+	INDEX (userId),
 	INDEX (blogTitle),
-	PRIMARY KEY (blogID),
-	FOREIGN KEY (userID) REFERENCES profile(userID),
-;
+	PRIMARY KEY (blogId),
+	FOREIGN KEY (userId) REFERENCES dcfProfile(userId)
+);
 
 CREATE TABLE blogComment (
-	commentId INT UNSIGNED AUTO-INCREMENT NOT NULL,
-	userId INT NOT NULL
-
-
-
-
-
+	commentId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	userId INT UNSIGNED NOT NULL,
+	blogId INT UNSIGNED NOT NULL,
+	timestamp TIMESTAMP,
+	comment VARCHAR (255) NOT NULL,
+	PRIMARY KEY (commentId),
+	INDEX (userId),
+	INDEX (blogId),
+	FOREIGN KEY (userId) REFERENCES dcfProfile(userId),
+	FOREIGN KEY (blogId) REFERENCES blogPost(blogId)
 );
 
 
