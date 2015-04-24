e708aa22e859ec114e25c218903f84797fc4b9d7
Jason Griffin <jasonmichaelgriffin@gmail.com>
2015-04-24 10:14:16 -0600

    Completed dcfprofile_test.php PDO configuration.  Still need to complete the actual class testing portion.
    Also need to do all of phase 3B in dcfprofile.php

--- php/classes/dcfprofile_test.php
+++ php/classes/dcfprofile_test.php
@@ -12,7 +12,7 @@ try {
 	$config = readConfig("/etc/apache2/data-design/jgriffin.ini");
 
 	//create a data connection string (DSN) & specify the user name and password
-	$dsn - "mysql:host=" . $config["hostname"] . ";dbname=" . $config["database"];
+	$dsn = "mysql:host=" . $config["hostname"] . ";dbname=" . $config["database"];
 
 	// enable UTF-8 (Unicode) text handling
 	$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
@@ -21,13 +21,17 @@ try {
 	$pdo = new PDO($dsn, $config["username"], $config["password"], $options);
 
 	//have PDO throw exceptions whenever possible
-	$pdo->setAttribute(PDO::ATTR_ERRMOD, PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 	//insert the stuff I'm testing here
-	// create a new DCF Profile
+	// create a new DCF Profile TODO: complete this section
 	$profile = new DcfProfile(1, "zaphod@heartofgold.com", "FroodyDude");
 
+	// Catch Exceptions.  TODO:  Here too shouldn't we be including the high level Exception catch in addition to PDOException?  Ask Dylan or Eric
+} catch(PDOException $pdoException) {
+	echo "Exception: " . $pdoException->getMessage();
 
 }
+//  TODO:  Ask why this closing tag is considered redundant by PHP
 ?>
